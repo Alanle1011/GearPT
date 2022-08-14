d3588b22595fb994f56b4b0918326663e11e1fa1
MarkTaylor1412 <minhtan14122013@gmail.com>
2022-08-14 20:51:32 +0700

    Update admin-login.blade.php

--- resources/views/Admin/admin-login.blade.php
+++ resources/views/Admin/admin-login.blade.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <title>Electro - HTML Ecommerce Template</title>
+    <title>Login, but for Admin - GEARPT</title>
+    <link rel="apple-touch-icon" href="img/Logo/2.png">
+    <link rel="shortcut icon" type="image/x-icon" href="img/Logo/2.png">
 
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
@@ -63,7 +65,7 @@
                     <div class="col-md-3">
                         <div class="header-logo">
                             <a href="{{url('/')}}" class="logo">
-                                <img src="./img/logo.png" alt="">
+                                <img src="./img/Logo/logo_gearpt.png" alt="">
                             </a>
                         </div>
                     </div>
